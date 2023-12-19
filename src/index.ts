59d21abf3c2a0cab734b4cd67c6324268d97c1be
Dasindu Hewagamage <dasinduhewagamage@gmail.com>
2023-12-19 22:45:26 +0530

    interface type adding

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,18 @@ let undefinedData: undefined = undefined;
 let animals: Array<string> = ['1', '2', '3'];
 let vehicles: string[] = ['Car', 'Bike', 'Lorry'];  //a string array
 let student: { name: string, age: number } = {name: 'Ranil', age: 90}; //an object with given types for properties
+
+
+
+// --------------------------------------------
+interface Animals {
+    name: string;
+    breed: string;
+    height: number
+}
+
+let dog: Animals = {name: 'Wolfy', breed: 'Husky', height: 1};
+console.log(dog.name);
+
+
+// --------------------------------------------
