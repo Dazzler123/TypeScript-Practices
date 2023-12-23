91020b4a24900f71351b72df57f704e6f3fbb5e6
Dasindu Hewagamage <dasinduhewagamage@gmail.com>
2023-12-23 19:16:43 +0530

    Introduced Object literals

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,14 @@
 "use strict";
+const studentObj = {
+    name: 'Mahinda',
+    age: 90,
+    address: {
+        city: 'Gampaha',
+        zipCode: '11400'
+    }
+};
+console.log(studentObj);
+console.log(studentObj.name);
+console.log(studentObj.address);
+console.log(studentObj.address.city);
+console.log(typeof studentObj);
